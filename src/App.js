5ac478c56f9646b8ecf3ddc71969ac771fffca16
Nihar Raote <nihar.raote@gmail.com>
2020-09-25 11:54:20 +0530

    Home component completed

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Home from './components/Home'
+
+const App = () => (
+    <div className="app">
+        <Home></Home>
+    </div>
+)
+
+export default App
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Home.css'
+
+const Home = () => (
+    <div className="home">
+        <h1>Create your own card, download and share it</h1>
+    </div>
+)
+
+export default Home
