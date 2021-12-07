a5b0618988df752f5fd1be595803197d66980425
Nihar Raote <nihar.raote@gmail.com>
2021-12-07 19:39:20 +0530

    Progress bar above stage components now changes color when selected stage changes

--- src/pages/GenerateCard.js
+++ src/pages/GenerateCard.js
@@ -7,16 +7,51 @@ import './GenerateCard.css'
 function GenerateCard() {
     const [cardComponent, changeCardComponent] = useState("msg")
 
-    let progressBarStyles = {
-        colorWidth: {
-            "width": "61%"
-        },
-        // add 4 boolean values; they correspond to the 4 milestones.
-        // if true, a class will be applied which holds the background color
-    }
+    let [width, setWidth] = useState({width: "21%"})
+
+    const [firstMilestone, setFirstMilestone] = useState("progress-milestone progress-milestone__color")
+    const [secondMilestone, setSecondMilestone] = useState("progress-milestone")
+    const [thirdMilestone, setThirdMilestone] = useState("progress-milestone")
+    const [fourthMilestone, setFourthMilestone] = useState("progress-milestone")
 
     const switchComponent = (componentName) => {
+        progressBarStyles(componentName)
         changeCardComponent(componentName)
+    }
+
+    const progressBarStyles = (componentStage) => {
+        switch(componentStage) {
+            case "msg":
+                setFirstMilestone("progress-milestone progress-milestone__color")
+                setSecondMilestone("progress-milestone")
+                setThirdMilestone("progress-milestone")
+                setFourthMilestone("progress-milestone")
+                setWidth({width: "21%"})
+                break
+            case "img":
+                setFirstMilestone("progress-milestone progress-milestone__color")
+                setSecondMilestone("progress-milestone progress-milestone__color")
+                setThirdMilestone("progress-milestone")
+                setFourthMilestone("progress-milestone")
+                setWidth({width: "41%"})
+                break
+            case "download":
+                setFirstMilestone("progress-milestone progress-milestone__color")
+                setSecondMilestone("progress-milestone progress-milestone__color")
+                setThirdMilestone("progress-milestone progress-milestone__color")
+                setFourthMilestone("progress-milestone")
+                setWidth({width: "61%"})
+                break
+            case "share":
+                setFirstMilestone("progress-milestone progress-milestone__color")
+                setSecondMilestone("progress-milestone progress-milestone__color")
+                setThirdMilestone("progress-milestone progress-milestone__color")
+                setFourthMilestone("progress-milestone progress-milestone__color")
+                setWidth({width: "81%"})
+                break
+            default:
+                console.log("Something weird is happening right now!!")
+        }
     }
 
     return (
@@ -24,12 +59,12 @@ function GenerateCard() {
             <div id="progress-indicator">
                 <div
                     id="progress-indicator-color"
-                    style={progressBarStyles.colorWidth}>
+                    style={width}>
                 </div>
-                <div className="progress-milestone"></div>
-                <div className="progress-milestone"></div>
-                <div className="progress-milestone"></div>
-                <div className="progress-milestone"></div>
+                <div className={firstMilestone}></div>
+                <div className={secondMilestone}></div>
+                <div className={thirdMilestone}></div>
+                <div className={fourthMilestone}></div>
             </div>
             <header id="progress-headings">
                 <CardStageContext.Provider value={switchComponent}>
