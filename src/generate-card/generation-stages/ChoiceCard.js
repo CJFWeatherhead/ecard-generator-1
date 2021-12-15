830d925fe147caeab1fa3006a1f32d0c2d2a8a54
Nihar Raote <nihar.raote@gmail.com>
2021-12-15 18:31:51 +0530

    Added card select UX and changed styles of download stage ecard
    dom-to-image now understands centering in download stage ecard now that flexbox is no longer used.
    
    Added a little UX for selected background image cards

--- src/generate-card/generation-stages/ChoiceCard.js
+++ src/generate-card/generation-stages/ChoiceCard.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './ChoiceCard.css'
 
 function ChoiceCard(props) {
+    let [classes, setClasses] = useState("message-card")
+    let [cardSelected, setCardSelected] = useState(true)
+
+    const addClassName = () => {
+        if (cardSelected) {
+            setClasses("message-card selected")
+            console.log("class selected added")
+        } else {
+            setClasses("message-card")
+        }
+        setCardSelected((prevState) => !prevState)
+    }
+
     return (
-        <div style={{backgroundColor: props.color, height: props.height, width: props.width}} className="message-card">
+        <div
+            style={{
+                    backgroundColor: props.color, 
+                    height: props.height, 
+                    width: props.width
+                    }}
+            className={classes}
+            onClick={addClassName}>
             <h3> {props.text} </h3>
         </div>
     )
